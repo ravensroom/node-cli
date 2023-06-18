2961857b26682436aee5ad4158107eee34d99076
raven <raven.lin.010@gmail.com>
2023-06-17 17:15:30 -0700

    Enpasulate createCLI

---
+++ ncli/packages/cli/lib/createCLI.js
@@ -0,0 +1,37 @@
+import { log, debug } from '@ncli/utils';
+import { program } from 'commander';
+import { readFileSync } from 'fs';
+import semver from 'semver';
+
+const pkg = JSON.parse(
+  readFileSync(new URL('../package.json', import.meta.url))
+);
+
+const MINIMUM_NODE_VERSION = '14.0.0';
+
+function checkNodeVersion() {
+  log.verbose('node version', process.version);
+  if (!semver.gte(process.version, MINIMUM_NODE_VERSION)) {
+    throw new Error(
+      chalk.red(
+        `ncli needs at least node of version ${MINIMUM_NODE_VERSION} ro run`
+      )
+    );
+  }
+}
+
+function preAction() {
+  // check node version
+  checkNodeVersion();
+}
+
+export default function createCLI() {
+  log.info('version', pkg.version);
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', 'Debug mode on', false)
+    .hook('preAction', preAction);
+  return program;
+}
--- ncli/packages/cli/lib/index.js
+++ ncli/packages/cli/lib/index.js
@@ -1,48 +1,17 @@
-import { program } from 'commander';
-import semver from 'semver';
 import createInitCommand from '@ncli/init';
-import { readFileSync } from 'fs';
 import { log, debug } from '@ncli/utils';
-
-const pkg = JSON.parse(
-  readFileSync(new URL('../package.json', import.meta.url))
-);
-
-const MINIMUM_NODE_VERSION = '14.0.0';
-
-function checkNodeVersion() {
-  log.verbose('node version', process.version);
-  if (!semver.gte(process.version, MINIMUM_NODE_VERSION)) {
-    throw new Error(
-      chalk.red(
-        `ncli needs at least node of version ${MINIMUM_NODE_VERSION} ro run`
-      )
-    );
-  }
-}
-
-function preAction() {
-  // check node version
-  checkNodeVersion();
-}
+import createCLI from './createCLI.js';
 
 process.on('uncaughtException', (e) => {
   if (debug()) {
-    console.log(e);
+    log.error(e);
   } else {
-    console.log(e.message);
+    log.error(e.message);
   }
 });
 
 function cli(args) {
-  log.info('version', pkg.version);
-  program
-    .name(Object.keys(pkg.bin)[0])
-    .usage('<command> [options]')
-    .version(pkg.version)
-    .option('-d, --debug', 'Debug mode on', false)
-    .hook('preAction', preAction);
-
+  const program = createCLI();
   createInitCommand(program);
 
   program.parse(process.argv);
